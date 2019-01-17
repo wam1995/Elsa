ef83284926d98bee1d4a95994974a0df783584e0
Maine <maine@MainedeMacBook-Pro.local>
2019-01-17 21:43:33 +0800

    html

--- functions.php
+++ functions.php
@@ -210,20 +210,6 @@ function elsa_keywords()
 }
 
 /**
- * page staticize
- *
- * @author Maine <https://www.cainiaofly.com>
- * @license GPL-3.0
- */
-function html_page_permalink() {
-    global $wp_rewrite;
-    if ( !strpos($wp_rewrite->get_page_permastruct(), '.html')){
-        $wp_rewrite->page_structure = $wp_rewrite->page_structure . '.html';
-    }
-}
-add_action('init', 'html_page_permalink', -1);
-
-/**
  * post thumbnail
  *
  * @author Maine <https://www.cainiaofly.com>
